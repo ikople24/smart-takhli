11db5d595890dc3ecdf093a6765c99f62f60f458
KOPLE <boldnessking26@gmail.com>
2025-05-30 10:51:38 +0700

    fix icons

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com", "storage.googleapis.com"],
+    domains: [
+      "res.cloudinary.com",
+      "storage.googleapis.com",
+      "cdn-icons-png.flaticon.com"
+    ],
   },
 };
 
