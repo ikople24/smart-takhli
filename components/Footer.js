bad9d956cd068b490c25111e19f5d53ee1fd7c2d
KOPLE <boldnessking26@gmail.com>
2025-06-12 10:21:17 +0700

    takhli clone is rady

--- components/Footer.js
+++ components/Footer.js
@@ -4,11 +4,11 @@ import { Globe, Youtube, Facebook } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="bg-white/30 text-gray-800 text-sm py-3 px-4 backdrop-blur-md border-t border-gray-300 flex justify-between items-center">
-      <div>© 2025 SMART-NAMPHRAE — All rights reserved</div>
+      <div>© 2025 SMART-TAKHLI — All rights reserved</div>
       <div className="flex gap-4 text-lg">
-        <a href="https://www.namphraephattana.go.th" className="hover:text-[#1DA1F2] transition-colors duration-200"><Globe size={20} strokeWidth={1.5} /></a>
-        <a href="https://www.youtube.com/@namphraephattana.office" className="hover:text-[#FF0000] transition-colors duration-200"><Youtube size={20} strokeWidth={1.5} /></a>
-        <a href="https://www.facebook.com/profile.php?id=100064091671258" className="hover:text-[#1877F2] transition-colors duration-200"><Facebook size={20} strokeWidth={1.5} /></a>
+        <a href="#" className="hover:text-[#1DA1F2] transition-colors duration-200"><Globe size={20} strokeWidth={1.5} /></a>
+        <a href="#" className="hover:text-[#FF0000] transition-colors duration-200"><Youtube size={20} strokeWidth={1.5} /></a>
+        <a href="#" className="hover:text-[#1877F2] transition-colors duration-200"><Facebook size={20} strokeWidth={1.5} /></a>
       </div>
     </footer>
   );
